c9307606dd799984b92afee5df5774cd01b08ac9
karthik nadar <karthiknadar1204@gmail.com>
2026-01-04 11:27:53 +0530

    gurads and rbac

---
+++ my-nest-app/src/guards/auth/auth.guard.ts
@@ -0,0 +1,17 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const authHeader = request.headers['authorization'];
+
+    return authHeader === 'Bearer my-secret-token';
+  }
+}
